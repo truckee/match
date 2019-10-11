03a87971efd1f052b358c9dfebe1aa9c10afc134
George Brooks <truckeetrout@yahoo.com>
2019-10-11 14:37:03 -0700

    add nonprofit landing page

---
+++ src/Controller/NonprofitController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * (c) GWB truckeesolutions@gmail.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+//src/Controller/NonprofitController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/nonprofit")
+ */
+class NonprofitController extends AbstractController
+{
+    /**
+     * @Route("/", name="nonprofit_index")
+     */
+    public function index() {
+        return $this->render('Nonprofit/index.html.twig');
+    }
+}
